177f1e5a7a8a06866f5c27c62a1b2aa0d1d06963
justEliada <eliadaballazhi@gmail.com>
2025-01-08 22:17:15 +0100

    creating layout for starting the project

---
+++ src/app/managment/managment.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-managment',
+  standalone: true,
+  imports: [],
+  templateUrl: './managment.component.html',
+  styleUrl: './managment.component.scss'
+})
+export class ManagmentComponent {
+
+}
---
+++ src/app/materials/materials.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-materials',
+  standalone: true,
+  imports: [],
+  templateUrl: './materials.component.html',
+  styleUrl: './materials.component.scss'
+})
+export class MaterialsComponent {
+
+}
